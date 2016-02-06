fc2f86842d5f8df79cfcaabaf72aaf9e08ae5e8a
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-02-06 14:35:54 +0200

    Rename ApplicationUser and ApplicationDbContext class and moved to GiftBox.Data.Models GiftBox.Data

---
+++ Source/GiftBox/Data/GiftBox.Data/GiftBoxDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace GiftBox.Data
+{
+    using GiftBox.Data.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class GiftBoxDbContext : IdentityDbContext<User>
+    {
+        public GiftBoxDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static GiftBoxDbContext Create()
+        {
+            return new GiftBoxDbContext();
+        }
+    }
+}
