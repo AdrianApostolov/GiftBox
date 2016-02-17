b39fa1f13879b054f6b6310ba9960ffff22ddf36
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-02-17 22:52:19 +0200

    Prepare project for unit testing and added two route tests

---
+++ Source/GiftBox/Tests/GiftBox.Web.Tests/RouteTests/GiftRouteTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Routing;
+using GiftBox.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcRouteTester;
+
+namespace GiftBox.Web.Tests.RouteTests
+{
+    [TestClass]
+    public class GiftRouteTests
+    {
+        [TestMethod]
+        public void TestGiftDonateGiftByIdWithValue()
+        {
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            var urlForTest = "/Gift/DonateGift/1";
+
+            routeCollection
+                .ShouldMap(urlForTest)
+                .To<GiftController>(x => x.DonateGift(1));
+        }
+
+        [TestMethod]
+        public void TestGiftDonateGiftByIdWithNull()
+        {
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            var urlForTest = "/Gift/DonateGift/1";
+
+            routeCollection
+                .ShouldMap(urlForTest)
+                .To<GiftController>(x => x.DonateGift(null));
+        }
+    }
+}
